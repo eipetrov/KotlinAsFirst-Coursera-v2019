75cefce966ee1036d5f5c4ace11993953f5351b1
eipetrov <e.petrov683@gmail.com>
2020-03-16 23:18:44 +0300

    Решены задачи: 1) Вычисление времени с начала суток в секундах; 2) Перевод градусов, минут, секунд в радианы; 3) Нахождение третьей цифры числа.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -48,9 +48,9 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 /**
  * Пример главной функции
  */
-fun main(args: Array<String>) {
-    val result =  seconds(8, 20, 35)
-    println("result = $result")
+fun main() {
+    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
+    println("Root product: $x1x2")
 }
 
 /**
@@ -59,13 +59,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    val a = hours * 60 * 60
-    val b = minutes * 60
-    val c = seconds
-    return a + b + c
-}
-
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
 
 
 /**
@@ -83,13 +77,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
-    return (summDegree(deg, min, sec) * PI) / 180
-}
-
-fun summDegree(deg: Int, min: Int, sec: Int): Double {
-    return deg + (min + (sec / 60.0)) / 60.0
-}
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
 
 /**
  * Тривиальная
@@ -105,17 +93,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int {
-    val a = number
-    val b = (a - firstDigit(number)) / 100
-    val c = b % 10
-    return c
-}
-
-fun firstDigit(number: Int): Int {
-    val a = number % 10
-    return a
-}
+fun thirdDigit(number: Int): Int = TODO()
 
 /**
  * Простая
