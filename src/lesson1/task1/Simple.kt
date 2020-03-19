e18d331970d9fef70a9e35cf81e181a117fbe4cc
eipetrov <e.petrov683@gmail.com>
2020-03-19 22:13:08 +0300

    Решена задача по переводу сажней, аршинов, вершков в сантиметры, а затем в метры

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,11 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val a = (sagenes * 3 * 16) + (arshins * 16) + vershoks
+    val b = a * 4.445
+    return b / 100
+}
 
 /**
  * Тривиальная
